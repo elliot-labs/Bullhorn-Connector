5177a4a2547d03f520133bde9361b1d24b677e1b
Elliot Huffman <ehuffman@elliot-labs.com>
2017-05-29 09:32:30 -0400

    add debugging, finalize get_command method
    Add debugging prints outs to each method. Debugging defaults are set to
    False. To use debugging, set debug=True on the method that you call.
    
    Finalized the get_command method so that you can use it generically for
    any call that requires HTTP GET.
    
    Add visual studio code launch.json so that efficient launches of python
    can be used (so it does nto pause on entry.)

--- BullhornConnector.py
+++ BullhornConnector.py
@@ -8,7 +8,7 @@ import requests
 
 class Authentication():
     """Provides the authentication strings needed for queries."""
-    def __init__(self):
+    def __init__(self, debug=False):
         """Sets the default settings for the authentication class"""
         self.authcode_url = 'https://auth.bullhornstaffing.com/oauth/authorize'
         self.authcode_data = {
@@ -27,20 +27,31 @@ class Authentication():
         self.rest_auth_data = {
             'version': '*',
             'access_token': self.get_token_data()}
+        if debug:
+            print(self.authcode_url)
+            print(self.authcode_data)
+            print(self.token_url)
+            print(self.token_url_data)
+            print(self.rest_auth_url)
+            print(self.rest_auth_data)
 
-    def get_authcode(self):
+    def get_authcode(self, debug=False):
         """Returns the value of the authentication code."""
         auth_code_request = requests.get(self.authcode_url, params=self.authcode_data)
         code = re.search('(?<=code=)[0-9%a-zA-Z-]+', auth_code_request.url)
+        if debug:
+            print(auth_code_request.url)
         return unquote(code.group(0))
 
-    def get_token_data(self):
+    def get_token_data(self, debug=False):
         """Returns the value of the token."""
         token_request = requests.post(self.token_url, params=self.token_url_data)
         token = re.search('(?<=\"access_token\" : \")[0-9]{2}:[0-9a-zA-Z-]+', token_request.text)
+        if debug:
+            print(token_request.url)
         return token.group(0)
 
-    def get_rest_access(self):
+    def get_rest_access(self, debug=False):
         """
         Returns the URL and token used to connect ot the rest api as a dictionary.
         The 'BhRestToken' index returns the rest API token.
@@ -48,6 +59,9 @@ class Authentication():
         """
         rest_access_response = requests.get(self.rest_auth_url, params=self.rest_auth_data)
         rest_access = json.loads(rest_access_response.text)
+        if debug:
+            print(rest_access_response.url)
+            print(rest_access)
         return rest_access
 
 
@@ -57,16 +71,30 @@ class DataAccess:
         self.authenticated_rest = Authentication()
         self.rest_access = self.authenticated_rest.get_rest_access()
 
-    def get_command(self, ):
-        """A test method"""
-        test_params = {
-            'BhRestToken': self.rest_access['BhRestToken'],
-            'fields': '*'}
-        result = requests.get(self.rest_access['restUrl'] + 'meta/Candidate', params=test_params)
-        print(result.url)
+    def get_command(self, urlpath, command_options=None, debug=False):
+        """A method that allows for dynamic get commands.\n
+        URL path is the argument that should be executed,\n
+        E.G. restURL/entity/Department?params\n
+        where entity/Department is the urlpath.\n
+        Command_options takes a dictionary"""
+
+        if command_options is None:
+            command_options = {}
+
+        get_command_params = {'BhRestToken': self.rest_access['BhRestToken']}
+        get_command_params.update(command_options)
+
+        result = requests.get(self.rest_access['restUrl'] + urlpath, params=get_command_params)
+
+        if debug:
+            print(command_options)
+            print(get_command_params)
+            print(result.url)
+
         return result.text
 
 
 if __name__ == '__main__':
     PRINTME = DataAccess()
-    print(PRINTME.get_command())
+    print(PRINTME.get_command('entity/Department/0'))
+    # {'fields': '*'}
