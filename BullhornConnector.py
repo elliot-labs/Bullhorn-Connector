09a42d571159dd905466751ebbfc3c059da246b6
Elliot Huffman <ehuffman@elliot-labs.com>
2017-06-02 11:09:49 -0400

    CLI Args
    Added teh ability to use command line arguments. Also rebuilt the debug
    system so that if the debug argument is specified it will properly
    output the debug information in a safe way. Added a shebang so that it
    can be run as a standalone program on a *nix based system. Added a
    docstring to the file itself for complience purposes.

--- BullhornConnector.py
+++ BullhornConnector.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3.5
+"""Provides data access to the bullhorn api.
+Returns the data as JSON formated text."""
+import argparse
 import json
 import re
 import time
@@ -9,9 +13,32 @@ import requests
 # The requests library is available via the PIP installer using:
 # "python -m pip install requests"
 
+
+def cli_args():
+    """Processes and provides CLI arguments."""
+    # Accept command line arguments.
+    parser = argparse.ArgumentParser()
+    # Add the entity command line argument.
+    parser.add_argument("entity", help="Sets the entity type that the API search should use. " +
+                        "E.G. 'JobSubmission'.")
+    # Add the fields command line argument.
+    parser.add_argument("fields",
+                        help="Sets the fields of the data that should be returned with a match. " +
+                        "Data should be a comma separated value " +
+                        "with no space on either side of the comma. " +
+                        "E.G. 'foo,bar,baz'.")
+    # Add the debug flag argument.
+    parser.add_argument("-d", "--debug", help="Enable debugging output", action="store_true")
+    # Process the arguments and make accessible
+    return parser.parse_args()
+
+
 class Authentication:
     """Provides the authentication strings needed for queries."""
-    def __init__(self, debug=False):
+
+    debug = False
+
+    def __init__(self):
         """Sets the default settings for the authentication class"""
         self.authcode_url = 'https://auth.bullhornstaffing.com/oauth/authorize'
         self.authcode_data = {
@@ -30,7 +57,7 @@ class Authentication:
         self.rest_auth_data = {
             'version': '*',
             'access_token': self.get_token_data()}
-        if debug:
+        if self.debug:
             print(self.authcode_url)
             print(self.authcode_data)
             print(self.token_url)
@@ -38,24 +65,24 @@ class Authentication:
             print(self.rest_auth_url)
             print(self.rest_auth_data)
 
-    def get_authcode(self, debug=False):
+    def get_authcode(self):
         """Returns the authcode.
         If no auth code is found, returns False."""
         auth_code_request = requests.get(self.authcode_url, params=self.authcode_data)
         code = re.search('(?<=code=)[0-9%a-zA-Z-]+', auth_code_request.url)
-        if debug:
+        if self.debug:
             print(auth_code_request.url)
         if code is None:
             return False
         else:
             return unquote(code.group(0))
 
-    def get_token_data(self, debug=False):
+    def get_token_data(self):
         """Returns the value of the auth token.
         If no token was found, returns false."""
         token_request = requests.post(self.token_url, params=self.token_url_data)
         token = re.search('(?<=\"access_token\" : \")[0-9]{2}:[0-9a-zA-Z-]+', token_request.text)
-        if debug:
+        if self.debug:
             print(token_request.url)
             print(token_request.text)
         if token is None:
@@ -63,7 +90,7 @@ class Authentication:
         else:
             return token.group(0)
 
-    def get_rest_access(self, debug=False):
+    def get_rest_access(self):
         """
         Returns the URL and token used to connect ot the rest api as a dictionary.
         The 'BhRestToken' index returns the rest API token.
@@ -71,7 +98,7 @@ class Authentication:
         """
         rest_access_response = requests.get(self.rest_auth_url, params=self.rest_auth_data)
         rest_access = json.loads(rest_access_response.text)
-        if debug:
+        if self.debug:
             print(rest_access_response.url)
             print(rest_access)
         return rest_access
@@ -79,13 +106,17 @@ class Authentication:
 
 class DataAccess:
     """Returns the requested data."""
+
+    debug = False
+
     def __init__(self):
         """Sets the required variables for all methods."""
 
         self.authenticated_rest = Authentication()
+        self.authenticated_rest.debug = self.debug
         self.rest_access = self.authenticated_rest.get_rest_access()
 
-    def get_command(self, urlpath, command_options=None, debug=False):
+    def get_command(self, urlpath, command_options=None):
         """A method that allows for dynamic get commands.\n
         URL path is the argument that should be executed,\n
         E.G. restURL/entity/Department?params\n
@@ -100,14 +131,14 @@ class DataAccess:
 
         result = requests.get(self.rest_access['restUrl'] + urlpath, params=get_command_params)
 
-        if debug:
+        if self.debug:
             print(command_options)
             print(get_command_params)
             print(result.url)
 
         return result.text
 
-    def api_search(self, entity='JobOrder', fields='*', debug=False):
+    def api_search(self, entity='JobOrder', fields='*'):
         """Runs a search command on all entries in the database.
         Returns the results of the search as JSON text."""
 
@@ -140,7 +171,7 @@ class DataAccess:
         results.pop("start")
         results.pop("count")
 
-        if debug:
+        if self.debug:
             print(search_params)
             print(search_request.url)
             print(original_search_request_parsed["count"])
@@ -150,6 +181,9 @@ class DataAccess:
         return json.dumps(results)
 
 if __name__ == '__main__':
+    COMMAND_LINE_ARGUMENTS = cli_args()
     PRINTME = DataAccess()
-    #print(PRINTME.api_search(fields='owner,clientCorporation,isOpen,title,submissions[0]'))
-    print(PRINTME.api_search(entity='JobSubmission', fields='candidate'))
+    PRINTME.debug = COMMAND_LINE_ARGUMENTS.debug
+    #fields='owner,clientCorporation,isOpen,title,submissions[0]'
+    print(PRINTME.api_search(entity='COMMAND_LINE_ARGUMENTS.entity',
+                             fields='COMMAND_LINE_ARGUMENTS.fields'))
